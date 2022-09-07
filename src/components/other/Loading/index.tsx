471c7091d1b938fb0b3fae6a606897fa2a1dc4f0
Job Klarenbeek <jjklarenbeek@hotmail.com>
2022-09-07 19:47:26 +0200

    loading rework

--- src/components/other/Loading/index.tsx
+++ src/components/other/Loading/index.tsx
@@ -1,20 +1,3 @@
 export const Loading: React.FC<{ bg: boolean }> = ({ bg }) => {
-	return (
-		<>
-			<div className={`loading ${bg ? "active" : ""}`.trim()}>
-				<div className="loading-Cirle-Wrapper">
-					<div className="loading-Circle main"></div>
-				</div>
-				<div className="loading-Cirle-Wrapper">
-					<div className="loading-Circle"></div>
-				</div>
-				<div className="loading-Cirle-Wrapper">
-					<div className="loading-Circle"></div>
-				</div>
-				<div className="loading-Cirle-Wrapper">
-					<div className="loading-Circle"></div>
-				</div>
-			</div>
-		</>
-	);
+	return <div className={`loader ${bg ? "active" : ""}`.trim()} />;
 };
